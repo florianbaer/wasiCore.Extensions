f6c8e14c89d77ba2d8673800f842e17b0f075e61
Thomas Waser <git@wasizug.ch>
2020-04-02 18:34:48 +0200

    init nuget Solution

---
+++ wasiCore.Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wasiCore.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Test(this string str, bool toUpper = false)
+        {
+            return toUpper ? str.ToUpper() : str;
+        }
+    }
+}

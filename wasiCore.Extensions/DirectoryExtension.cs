c513444c2b5ffff9e8c3880ec5af1a4ff6bfe9a2
Thomas Waser <git@wasizug.ch>
2020-04-02 22:24:56 +0200

    added File and Directory Extensions. Juhuuu

---
+++ wasiCore.Extensions/DirectoryExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace wasiCore.Extensions
+{
+    public static class DirectoryExtension
+    {
+        public static void Create(string directory)
+        {
+            if (string.IsNullOrEmpty(Path.GetExtension(directory)))
+                directory += @"\";
+
+            var directoryPath = Path.GetDirectoryName(directory);
+            var dir = directoryPath?.Split('\\').First();
+
+            directoryPath?.Split('\\').Skip(1).ToList().ForEach(item =>
+            {
+                dir += $@"\{item}";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+            });
+        }
+
+
+
+
+        public static void Delete(string path, bool deleteRecursive = false)
+        {
+            if (!string.IsNullOrEmpty(Path.GetExtension(path))) return;
+            
+            path += @"\";
+
+            if (deleteRecursive)
+            {
+                Directory.GetFiles(path).ToList().ForEach(file =>
+                {
+                    file.Delete();
+                });
+                Directory.GetDirectories(path).ToList().ForEach(dir =>
+                {
+                    Delete(dir, true);
+                });
+
+            }
+            else if (!Directory.GetDirectories(path).Any() && !Directory.GetFiles(path).Any())
+            {
+                Directory.Delete(path);
+            }
+        }
+
+    }
+}
---
+++ wasiCore.Extensions/FileExtension.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace wasiCore.Extensions
+{
+    public static class FileExtension
+    {
+
+
+        public static FileStream Create(string path)
+        {
+            DirectoryExtension.Create(path);
+            return File.Create(path);
+        }
+
+
+
+        public static void Delete(this string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch { }
+        }
+
+
+        public static void Delete(this string path, out string errorMessage)
+        {
+            errorMessage = "";
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+
+        public static void Copy(string sourceFileName, string destFileName, bool overwrite = false)
+        {
+            if (!File.Exists(sourceFileName)) return;
+            DirectoryExtension.Create(destFileName);
+            File.Copy(sourceFileName, destFileName, overwrite);
+        }
+
+
+
+        public static void Move(string sourceFileName, string destFileName, bool overwrite = false)
+        {
+            if (!File.Exists(sourceFileName)) return;
+            DirectoryExtension.Create(destFileName);
+            if (!overwrite && File.Exists(destFileName)) return;
+            if (File.Exists(destFileName))
+                File.Delete(destFileName);
+            File.Move(sourceFileName, destFileName);
+        }
+
+
+
+        public static void AppendAllText(string path, string contents, Encoding encoding = null)
+        {
+            DirectoryExtension.Create(path);
+            if (encoding is null)
+                File.AppendAllText(path, contents);
+            else
+                File.AppendAllText(path, contents, encoding);
+        }
+
+
+
+
+        public static void AppendAllLines(string path, IEnumerable<string> contents, Encoding encoding = null)
+        {
+            DirectoryExtension.Create(path);
+            if (encoding is null)
+                File.AppendAllLines(path, contents);
+            else
+                File.AppendAllLines(path, contents, encoding);
+        }
+
+
+
+
+        public static string[] ReadAllLines(string path, Encoding encoding = null)
+        {
+            return !File.Exists(path) ? new string[] { } : File.ReadAllLines(path, encoding ?? Encoding.UTF8);
+        }
+
+
+
+
+
+        public static string ReadText(string path, Encoding encoding = null)
+        {
+            return !File.Exists(path) ? null : File.ReadAllText(path, encoding ?? Encoding.UTF8);
+        }
+
+
+
+
+
+        public static byte[] ReadAllBytes(string path)
+        {
+            return !File.Exists(path) ? new byte[] { } : File.ReadAllBytes(path);
+        }
+
+
+
+
+        public static IEnumerable<string> ReadLines(string path, Encoding encoding = null)
+        {
+            return !File.Exists(path) ? new List<string>() : File.ReadLines(path, encoding ?? Encoding.UTF8);
+        }
+
+    }
+}
